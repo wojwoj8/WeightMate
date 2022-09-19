5c0c6f90918ea6180f88cf9917cb2a6e8f6e360d
wojwoj8 <109290484+wojwoj8@users.noreply.github.com>
2022-09-19 23:08:33 +0200

    Add files via upload

---
+++ static/js/validate.js
@@ -0,0 +1,31 @@
+function validateForm(){
+    var usern = document.forms["registerform"]["username"].value;
+    var passw = document.forms["registerform"]["password"].value;
+    var log = !! document.getElementById("confirmation");
+    if (log == true){
+        var confirm = document.forms["registerform"]["confirmation"].value;
+        }
+    if(usern == null || usern == ""){
+        document.getElementsByClassName("error")[0].style.visibility = "visible";
+        document.getElementsByClassName("authstyle")[0].style.border = "2px solid red";
+        document.getElementsByClassName("error")[0].innerHTML = "Please Fill out all fields";
+        return false;
+    }
+   else if(passw == null || passw == ""){
+        document.getElementsByClassName("error")[0].style.visibility = "visible";
+        document.getElementsByClassName("authstyle")[1].style.border = "2px solid red";
+        document.getElementsByClassName("error")[0].innerHTML = "Please Fill out password";
+        return false;
+   }
+   if (log == true){
+
+        if(confirm != passw){
+        document.getElementsByClassName("error")[0].style.visibility = "visible";
+        document.getElementsByClassName("authstyle")[2].style.border = "2px solid red";
+        document.getElementsByClassName("error")[0].innerHTML = "passwords must be the same";
+        return false;
+    }
+        } else {
+        return true;
+    }
+}
